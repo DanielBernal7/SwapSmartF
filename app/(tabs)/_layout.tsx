67667d39702440d940fb05255c76965c0303455f
DanielBernal7 <danbernal@csumb.edu>
2026-02-24 23:45:30 -0800

    Formattnig changes.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,7 +5,6 @@ import { NativeTabs, Icon, Label } from "expo-router/unstable-native-tabs";
 export default function TabLayout() {
 	return (
 		<NativeTabs>
-
 			<NativeTabs.Trigger name="index">
 				<Label>Home</Label>
 				<Icon sf="house.fill" />
@@ -13,14 +12,13 @@ export default function TabLayout() {
 
 			<NativeTabs.Trigger name="explore">
 				<Label>Explore</Label>
-				<Icon sf="paperplane.fill"/>
+				<Icon sf="paperplane.fill" />
 			</NativeTabs.Trigger>
 
 			<NativeTabs.Trigger name="scanner">
 				<Label>Scan</Label>
-				<Icon sf="barcode.viewfinder"/>
+				<Icon sf="barcode.viewfinder" />
 			</NativeTabs.Trigger>
-
 		</NativeTabs>
 	);
 }
