96a17bdd6fa65c9cb03b30ba48f341208bee39a1
DanielBernal7 <danbernal@csumb.edu>
2026-02-24 23:35:21 -0800

    Moved the tabs from using regualar tab to now using NativeTabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,10 +2,6 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
-import { NativeTabs, Icon, Label } from 'expo-router/unstable-native-tabs';
-
-
-
 import { useColorScheme } from "@/hooks/use-color-scheme";
 
 export const unstable_settings = {
